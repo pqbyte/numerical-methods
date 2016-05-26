d7fb310e9b67a4e6dcf79c641c7e975a93eb3032
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-26 18:17:43 +0200

    Add newton's method

---
+++ newtons.m
@@ -0,0 +1,22 @@
+% Newton-Raphsons method for function with 4 roots.
+
+f = @(x) (x-1)^2;
+fp = @(x) 2*(x-1);
+
+TOL = 1e-10;
+err = inf;
+iter = 1;
+
+% Starting guess
+x = 1.00001;
+
+disp('x               f(x)            relative error')
+while err > TOL & iter <= 20
+  dx = -f(x)/fp(x);
+  x = x+dx;
+  err = abs(dx/x);
+  fprintf('%.12f\t%.12f\t%.12f\n', [x, f(x), err])
+  iter = iter+1;
+end
+
+disp(['Iterations: ' num2str(iter)])
