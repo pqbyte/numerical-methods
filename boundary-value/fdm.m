189268155361d9c1e8977ad363ce3c01665a807d
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-31 18:52:41 +0200

    Make fdm more consistent

--- boundary-value/fdm.m
+++ boundary-value/fdm.m
@@ -1,46 +1,40 @@
 % Finite difference method on y''+y'sqrt(x)=(6-y)x
 % y(0)=1; y(4)=-1
 
-% Known values
-a = 0;
-b = 4;
-ya = 1;
-yb = -1;
+a = 0;            % Starting x-value
+b = 4;            % Ending x-value
+ya = 1;           % Starting y-value
+yb = -1;          % Ending y-value
+n = 20;           % Intervals
+h = (b-a)/n;      % Step size
+N = (n+1)-2;      % Unknowns
+X = (a:h:b)';     % Set of x-values
+x = X(2:N+1);     % Values used for solving y
 
-% Properties of the problem
-N = 100;       % # of intervals
-h = (b-a)/N;
-n = (N+1)-2;   % # of unknowns
-x = h*(1:n)';
-
-% Function
+% Functions from discretization of ODE
 f1 = @(x) 1-h*sqrt(x)/2;
 f2 = @(x) -(2-h^2*x);
 f3 = @(x) 1+h*sqrt(x)/2;
 g = @(x) 6*h^2*x;
 
 % Fill tridiagonal matrix
-A = zeros(n);
-for i = 1:n
-  A(i, i) = f2(x(i));     % diagonal
+A = zeros(N);
+for i = 1:N
+  A(i,i) = f2(x(i));     % diagonal
 end
-for i = 1:n-1
-  A(i, i+1) = f3(x(i));   % super-diagonal
-  A(i+1, i) = f1(x(i+1)); % sub-diagonal
+for i = 1:N-1
+  A(i,i+1) = f3(x(i));   % super-diagonal
+  A(i+1,i) = f1(x(i+1)); % sub-diagonal
 end
 
-% Right side
+% Right side of equality
 c = g(x);
 c(1) = c(1)-f1(x(1))*ya;
-c(n) = c(n)-f3(x(n))*yb;
+c(N) = c(N)-f3(x(N))*yb;
 
 % Solution
 y = A\c;
-
-% Plot
-X = [a; x; b];
 Y = [ya; y; yb];
-plot(X, Y)
 
-disp('    min-y    max-y')
-disp([min(y)    max(y)])
+disp('    x               y')
+fprintf('%.6f\t%.6f\n', X, Y)
