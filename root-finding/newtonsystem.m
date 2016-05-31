c481696ccc6e90b8aec28939f45b17e31974ca2c
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-31 17:43:29 +0200

    Simplify newtonsystem

--- root-finding/newtonsystem.m
+++ root-finding/newtonsystem.m
@@ -1,4 +1,5 @@
-% Multivariable Newton's method.
+% Newton's method on system of equations.
+% f(x)=0, x=[x1 x2].
 
 f = @(x) [x(1)+x(2)^2;
         -1+x(1)^2+4*x(2)];
@@ -7,19 +8,23 @@ f = @(x) [x(1)+x(2)^2;
 J = @(x) [1, 2*x(2);
           2*x(1), 4];
 
-TOL = 1e-14;
-h = inf;
-iter = 1;
+TOL = 1e-10; % Error limit
+MIT = 100;   % Maximum iterations
+err = inf;   % Max norm error
+iter = 0;    % Current iteration
+x = [0 1]';  % Starting guesses
 
-% Starting guesses
-x = [-0.062 0.25]';
-
-disp('x               y               error')
-while (norm(h) > TOL)
+disp('      x1              x2             error')
+while err > TOL && iter < MIT
   h = -J(x)\f(x);
   x = x+h;
-  fprintf('%.12f\t%.12f\t%.12f\n', [x(1), x(2), norm(h)])
+  err = norm(h,inf);
   iter = iter+1;
+  fprintf('%.12f\t%.12f\t%.12f\n', x(1), x(2), err)
 end
 
-disp(['Iterations: ' num2str(iter)])
+if (iter == MIT)
+  disp('Did not converge.')
+else
+  fprintf('%s %d %s\n','Converged. Result after',iter,'iterations.')
+end
