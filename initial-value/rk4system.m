4000c75e376ecbf04f49aad601946f5e5e9d8979
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-31 14:58:02 +0200

    Simplify rk4system

--- initial-value/rk4system.m
+++ initial-value/rk4system.m
@@ -1,32 +1,25 @@
 % RK4 on system of equations.
+% y'=f(x,y), y=[y1 y2], y(a)=[y01 y02].
 
-f1 = @(x, y) y(2)^2-2*y(1);      % EQ1
-f2 = @(x, y) y(1)-y(2)-x*y(2)^2; % EQ2
-f =  @(x, y) [f1(x,y) f2(x,y)];  % System
+f = @(x,y) [y(2)^2-2*y(1);
+            y(1)-y(2)-x*y(2)^2];
 
-a = 0;        % Lower bound
-b = 3;        % Upper bound
-h = 0.01;     % Step size
-n = (b-a)/h;  % Intervals
+a = 0;       % Starting x-value
+b = 3;       % Target x-value
+h = 0.1;     % Step size
+n = (b-a)/h; % Intervals
+y = [1 0]';  % Starting y-values
+X = a:h:b;   % Set of x-values
 
-X = zeros(n+1, 1);
-Y = zeros(n+1, 2);
-X(1) = a;         % Starting x-value
-Y(1, :) = [1 0];  % Starting y-values.
-
-disp('   x               y1              y2')
-fprintf('%.6f\t%.6f\t%.6f\n', X(1), Y(1, :))
+disp('    x              y1              y2')
+fprintf('%.6f\t%.6f\t%.6f\n',X(1),y)
 
 for i = 1:n
   x = X(i);
-  y = Y(i, :);
-  f1 = f(x, y);
-  f2 = f(x+h/2, y+h*f1/2);
-  f3 = f(x+h/2, y+h*f2/2);
-  f4 = f(x+h, y+h*f3);
-  Y(i+1, :) = y+h*(f1+2*f2+2*f3+f4)/6;
-  X(i+1) = x+h;
-  fprintf('%.6f\t%.6f\t%.6f\n', X(i+1), Y(i+1, :))
+  k1 = f(x,y);
+  k2 = f(x+h/2, y+h*k1/2);
+  k3 = f(x+h/2, y+h*k2/2);
+  k4 = f(x+h, y+h*k3);
+  y = y+h*(k1+2*k2+2*k3+k4)/6;
+  fprintf('%.6f\t%.6f\t%.6f\n',X(i+1),y)
 end
-
-plot(X, Y);
