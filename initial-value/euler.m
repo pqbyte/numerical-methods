d9f9ee90e8c143a00ff5dc463ebf9f7ae6476adf
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-31 14:07:55 +0200

    Simplify euler

--- initial-value/euler.m
+++ initial-value/euler.m
@@ -1,24 +1,17 @@
-% Euler's method on y'=3e^(-x)-0.4y, y(0)=5.
+% Euler's method on y'=f(x,y), y(a)=y0.
 
 f = @(x,y) 3*exp(-x)-0.4*y;
 
-a = 0;       % Lower bound
-b = 3;       % Upper bound
-h = 1.5;     % Step size
+a = 0;       % Starting x-value
+b = 3;       % Target x-value
+h = 0.1;     % Step size
 n = (b-a)/h; % Intervals
-
-X = zeros(n+1, 1);
-Y = zeros(n+1, 1);
-X(1) = a; % Starting x-value
-Y(1) = 5; % Starting y-value
+y = 5;       % Starting y-value
+X = a:h:b;   % Set of x-values
 
 disp('   x               y')
-fprintf('%.6f\t%.6f\n', X(1), Y(1))
-
-for i = 1:n
+for i = 1:n+1
   x = X(i);
-  y = Y(i);
-  Y(i+1) = y+h*f(x,y);
-  X(i+1) = x+h;
-  fprintf('%.6f\t%.6f\n', X(i+1), Y(i+1))
+  y = y+h*f(x,y);
+  fprintf('%.6f\t%.6f\n', x, y)
 end
