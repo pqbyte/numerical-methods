48710b4bd58a2e33c2f68b164cdb4870955c6215
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-31 17:57:44 +0200

    Make secant more consistent

--- root-finding/secant.m
+++ root-finding/secant.m
@@ -1,24 +1,26 @@
-% Secant method.
+% Secant method to find roots of f(x)=0.
 
 f = @(x) (x-1)^2;
 
-TOL = 1e-10;
-err = inf;
-iter = 1;
+TOL = 1e-10;  % Error limit
+MIT = 100;    % Maximum iterations
+err = inf;    % Absolute error
+iter = 0;     % Current iteration
+x0 = 1.01;    % Starting guess 1
+x1 = 1.02;    % Starting guess 2
 
-% Starting guesses
-x0 = 1.00001;
-x1 = 1.00002;
-
-disp('x               f(x)            absolute error')
+disp('       x             f(x)       absolute error')
 while err > TOL && iter < 100
-  dx = -f(x1)*(x1-x0)/(f(x1)-f(x0));
+  h = -f(x1)*(x1-x0)/(f(x1)-f(x0));
   x0 = x1;
-  x1 = x1+dx;
+  x1 = x1+h;
   err = abs(x1-x0);
   iter = iter+1;
-  fprintf('%.12f\t%.12f\t%.12f\n', [x1, f(x1), err])
+  fprintf('%.12f\t%.12f\t%.12f\n', x1, f(x1), err)
 end
-x = x1;
 
-disp(['Iterations: ' num2str(iter)])
+if (iter == MIT)
+  disp('Did not converge.')
+else
+  fprintf('%s %d %s\n','Converged. Result after',iter,'iterations.')
+end
