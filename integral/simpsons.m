f74de84f3733eb02424cd77bcf30e7c3e62ef3e6
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-31 16:09:53 +0200

    Simplify simpsons

--- integral/simpsons.m
+++ integral/simpsons.m
@@ -1,21 +1,13 @@
-% Simpsons rule for integral of function f(x) = 1+x².
+% Simpsons rule for approximating ∫f(x)dx.
 
-si = @(h, y) h*(y(1)+4*sum(y(2:2:end))+2*sum(y(3:2:end-2))+y(end))/3;
 f = @(x) 1+x.^2;
 
-% True value of integral
-Itrue = 136/3;
-fprintf('True value of integral = %.6f\n', Itrue)
-fprintf('\n')
-
-a = 1;       % Lower bound
-b = 5;       % Upper bound
-n = 4;       % Segments, must be even.
+a = 1;       % Lower integral bound
+b = 5;       % Upper integral bound
+n = 4;       % Intervals
 h = (b-a)/n; % Step size
+X = a:h:b;   % Set of x-values
+Y = f(X);    % Set of y-values
 
-x = a:h:b;
-y = f(x);
-Iappr = si(h, y);
-err = abs(Iappr-Itrue);
-disp('h               integral        absolute error')
-fprintf('%.6f\t%.6f\t%.6f\n', [h, Iappr, err]')
+Iappr = h*(Y(1)+4*sum(Y(2:2:end))+2*sum(Y(3:2:end-2))+Y(end))/3;
+fprintf('%s %.6f\n', 'Integral ≈', Iappr)
